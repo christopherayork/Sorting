4b04faf2efd8749080cd1dcfa8aca13a9ecaf493
Chris York <christopherayork1992@gmail.com>
2019-12-09 13:53:28 -0500

    MVP Done

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,37 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
-    
-    return merged_arr
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+def merge(left, right):
+    result = []
+    while len(left) or len(right):
+        if len(left) <= 0:
+            result.append(right.pop(0))
+        elif len(right) <= 0:
+            result.append(left.pop(0))
+        elif left[0] <= right[0]:
+            result.append(left.pop(0))
+        elif left[0] >= right[0]:
+            result.append(right.pop(0))
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
+import math
 
-    return arr
+
+def merge_sort(l):
+    if len(l) <= 1: return l
+    middle = math.floor(len(l) / 2)
+    left = merge_sort(l[0:middle])
+    right = merge_sort(l[middle:])
+    result = []
+    while len(left) or len(right):
+        if len(left) <= 0:
+            result.append(right.pop(0))
+        elif len(right) <= 0:
+            result.append(left.pop(0))
+        elif left[0] <= right[0]:
+            result.append(left.pop(0))
+        elif left[0] >= right[0]:
+            result.append(right.pop(0))
+    return result
 
 
 # STRETCH: implement an in-place merge sort algorithm
